ba2d43c08621111b5ea1c50c64fd6cc3decd2a67
Zhakhangir <jahangirtairov123@gmail.com>
2020-05-18 12:58:13 +0600

    nstalled npm and modified index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom';  
+
+let App = ()=>{
+  return   <h1> Hello World </h1>;
+};
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ 
